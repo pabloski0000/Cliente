676358cf72c24b2352ee98f917581efd568a414f
pablo m <plajommj@hotmail.com>
2020-11-30 15:26:51 +0100

    Hecho el ataque, ahora empezaré con las colisiones y el movimiento de la máquina

--- Unidad 2/Juego/javascript/juego.js
+++ Unidad 2/Juego/javascript/juego.js
@@ -1,14 +1,15 @@
 window.onload = function() {
-	
+	let c = 0;
 	let TAMAÑONAVEX = 40;
 	let TAMAÑONAVEY = 40;
 	const ARRAYPOKEMON = [];
+	const ARRAYATAQUES = [];
 	let cont = 0, numeroAleatorioIA;
 	
 	const VELOCIDADPIKACHU = 3, VELOCIDADRAICHU = 5;
 	
 	const YMAQUINA = 35;
-	const Y = 425;   // posición inicial y del rectángulo
+	const YJUGADOR = 425;   // posición inicial y del rectángulo
 	let canvas;  // variable que referencia al elemento canvas del html
 	let ctx;     // contexto de trabajo
 	var id;      // id de la animación
@@ -19,29 +20,51 @@ window.onload = function() {
 	
 	var animacionComecocos = [[0,0],[32,0],[0,65],[32,65]]; // Posiciones del sprite donde recortar cada imagenn 
 	//(0 cerrado derecha, 1 abierto derecha, 2 cerrado izquierda, 3 cerrado izquierda)
-	var posicion=0;                                         // Posición del array 0, 1, 2, 3
+	/* provisional -> */ let posicion = 0;                                         // Posición del array 0, 1, 2, 3
 	var ejecutarDerecha = true, ejecutarIzquierda = false;  // Control de cambio de posicion derecha - izquierda y viceversa
 
 	let imagenPikachu, imagenRayoPikachu;
 
-	function ObjetoBatalla(x_, y_,imagen,posicionImagenX,posicionImagenY){
+	function ObjetoBatalla(x_, y_,velocidad,imagen,posicionImagenX,posicionImagenY,anchoDibujo,altoDibujo,anchoImagen,altoImagen){
 		this.x = x_;
-	    this.y = y_;
-		this.posicionImagenX = posicionImagenX;
-		this.posicionImagenY = posicionImagenY;
+		this.y = y_;
+		this.velocidad = velocidad;
+		this.arrayImagenes = [[posicionImagenX,posicionImagenY]];
 		this.imagen = imagen;
+		this.anchoDibujo = anchoDibujo;
+		this.altoDibujo = altoDibujo;
+		this.anchoImagen = anchoImagen;
+		this.altoImagen = altoImagen;
 	}
 	
 	//La variable pokemonDeUsuario será booleana
-	function Pokemon (x_, y_,velocidad,imagen,posicionImagenX,posicionImagenY){
+	function Ataque(x_, y_,velocidad,imagen,posicionEnDibujoX,posicionEnDibujoY,anchoDibujo,altoDibujo,anchoImagen,altoImagen,posicionImagenX2,posicionImagenY2){
 		this.base = ObjetoBatalla;
-		this.base(x_,y_,imagen,posicionImagenX,posicionImagenY);
-		this.velocidad = velocidad;
+		this.base(x_,y_,velocidad,imagen,posicionEnDibujoX,posicionEnDibujoY,anchoDibujo,altoDibujo,anchoImagen,altoImagen);
+		this.arrayImagenes.push([posicionImagenX2,posicionImagenY2]);
+	}
+
+	Ataque.prototype = new ObjetoBatalla();
+
+	function Pokemon(x_, y_,velocidad,imagen,posicionEnDibujoX,posicionEnDibujoY,anchoDibujo,altoDibujo,anchoImagen,altoImagen){
+		this.base = ObjetoBatalla;
+		this.base(x_, y_,velocidad,imagen,posicionEnDibujoX,posicionEnDibujoY,anchoDibujo,altoDibujo,anchoImagen,altoImagen);
+
+		this.lanzarAtaque = function(){
+			let rayoDeLaMuerte = new Ataque(this.x + 10,this.y,9,imagenRayoPikachu,0,0,35,80,20,45,35,0);
+			ARRAYATAQUES.push(rayoDeLaMuerte);
+			console.log(this.x);
+			console.log(this.y);
+		}
 	}
 
 	Pokemon.prototype = new ObjetoBatalla();
-	
-	Pokemon.prototype.generaPosicionDerecha = function() {
+
+	ObjetoBatalla.prototype.generaPosicionArriba = function() {
+		this.y = this.y - this.velocidad;	
+	}
+
+	ObjetoBatalla.prototype.generaPosicionDerecha = function() {
 		
 		this.x = this.x + this.velocidad;
 		
@@ -54,7 +77,7 @@ window.onload = function() {
 	}
 	
 	
-	Pokemon.prototype.generaPosicionIzquierda = function() {
+	ObjetoBatalla.prototype.generaPosicionIzquierda = function() {
 		
 		this.x = this.x - this.velocidad;
 		
@@ -66,16 +89,16 @@ window.onload = function() {
 		
 	}
 	
-	Pokemon.prototype.pintarPokemon = function(){
+	ObjetoBatalla.prototype.pintarObjeto = function(){
 		ctx.drawImage(this.imagen, // Imagen completa con todos los comecocos (Sprite)
-		this.posicionImagenX,    // Posicion X del sprite donde se encuentra el comecocos que voy a recortar del sprite para dibujar
-		this.posicionImagenY,	  // Posicion Y del sprite donde se encuentra el comecocos que voy a recortar del sprite para dibujar
-		TAMAÑONAVEX, 		  // Tamaño X del comecocos que voy a recortar para dibujar
-		TAMAÑONAVEY,	      // Tamaño Y del comecocos que voy a recortar para dibujar
+		this.arrayImagenes[posicion][0],    // Posicion X del sprite donde se encuentra el comecocos que voy a recortar del sprite para dibujar
+		this.arrayImagenes[posicion][1],	  // Posicion Y del sprite donde se encuentra el comecocos que voy a recortar del sprite para dibujar
+		this.anchoDibujo, 		  // Tamaño X del comecocos que voy a recortar para dibujar
+		this.altoDibujo,	      // Tamaño Y del comecocos que voy a recortar para dibujar
 		this.x,      // Posicion x de pantalla donde voy a dibujar el comecocos recortado
 		this.y,				  // Posicion y de pantalla donde voy a dibujar el comecocos recortado
-		TAMAÑONAVEX,		  // Tamaño X del comecocos que voy a dibujar
-		TAMAÑONAVEY);       // Tamaño Y del comecocos que voy a dibujar
+		this.anchoImagen,		  // Tamaño X del comecocos que voy a dibujar
+		this.altoImagen);       // Tamaño Y del comecocos que voy a dibujar
 	}
 	
 	function pintaRectangulo() {
@@ -87,9 +110,11 @@ window.onload = function() {
 		
 		// Pintamos a Pokemon
 		
-		ARRAYPOKEMON[0].pintarPokemon();
-		ARRAYPOKEMON[1].pintarPokemon();
-		/*rayoDeLaMuerte.pintar();*/
+		ARRAYPOKEMON[0].pintarObjeto();
+		ARRAYPOKEMON[1].pintarObjeto();
+		for(i in ARRAYATAQUES){
+			ARRAYATAQUES[i].pintarObjeto();
+		}
 	}
 	
 	function generarPosiciones(){
@@ -113,6 +138,11 @@ window.onload = function() {
 		if(xIzquierdaMaquina){
 			ARRAYPOKEMON[1].generaPosicionIzquierda();
 		}
+		
+		for(i in ARRAYATAQUES){
+			ARRAYATAQUES[i].generaPosicionArriba();
+		}
+
 	}
 	
 	function abreCierraBoca() {
@@ -162,46 +192,59 @@ window.onload = function() {
 		}
 	}
 	
-	function activaMovimiento(evt) {
+	function activaFuncionalidad(evt) {
 		
 		switch (evt.keyCode) {
-			
-			// Left arrow.
 			case 37: 
 				xIzquierda = true;
 				activarIzquierda();
-				break;
+			break;
+
+			case 39:
+				xDerecha = true;
+				activarDerecha();
+			break;				
+
+			/*case 32:
+				ARRAYPOKEMON[0].lanzarAtaque();
+			break;*/
 			
-				// Right arrow.
-				case 39:
-					xDerecha = true;
-					activarDerecha();
-					break;
-					
-				}
-			}
-			
-			function desactivaMovimiento(evt){
+			default:
 				
-				switch (evt.keyCode) {
-					
-					// Left arrow
-					case 37: 
-					xIzquierda = false;
-					desactivarIzquierda();
-					break;
-					
-					// Right arrow 
-					case 39:
+		}
+	}
+		
+	function desactivaFuncionalidad(evt){
+			
+		switch (evt.keyCode) {
+			case 37: 
+				xIzquierda = false;
+				desactivarIzquierda();
+			break;
+				
+			case 39:
 				xDerecha = false;
 				desactivarDerecha();
-				break;
-				
-			}
-			
-		}
+			break;
 
-		function movimientoIA(){
+			/*case 32:
+				ARRAYPOKEMON[0].lanzarAtaque();
+			break;*/
+					
+			default:
+
+		}	
+	}
+		
+	function activarAtaque(event){
+		switch (event.keyCode){
+			case 32:
+				ARRAYPOKEMON[0].lanzarAtaque();
+			break;
+		}
+	}
+
+	function movimientoIA(){
 		numeroAleatorioIA = Math.round(Math.random());
 		console.log(numeroAleatorioIA);
 		if(numeroAleatorioIA == 0){
@@ -213,8 +256,9 @@ window.onload = function() {
 		}
 	}
 		
-	document.addEventListener("keydown", activaMovimiento, false);
-	document.addEventListener("keyup", desactivaMovimiento, false);	
+	document.addEventListener("keydown", activaFuncionalidad, false);
+	document.addEventListener("keyup", desactivaFuncionalidad, false);
+	document.addEventListener("keypress", activarAtaque,false);
 	
 	// localizamos el canvas
 	canvas = document.getElementById("main-canvas");
@@ -225,17 +269,19 @@ window.onload = function() {
 	imagenPikachu = new Image();
 	imagenPikachu.src="../css/imagenes/sprites de prueba de pikachu.png";
 	imagenRayoPikachu = new Image();
-	imagenRayoPikachu.src = '../css/imagenes/sprites de pikachu_batalla.png';
+	imagenRayoPikachu.src = '../css/imagenes/Prototipo-ataque-pikachu_pasado-por-photoshop.png';
 	
-	let rayoDeLaMuerte = new ObjetoBatalla(0,0,imagenRayoPikachu,0,0);
 	
-	//Declaramos los tres pokemons
-	let pikachu = new Pokemon(Math.random() * 460, Y, VELOCIDADPIKACHU,imagenPikachu,205,205);
-	let pikachuIA = new Pokemon(Math.random() * 460, YMAQUINA, VELOCIDADPIKACHU,imagenPikachu,10,20);
+	if(true){
+		let pikachuJugador = new Pokemon(Math.random() * 460, YJUGADOR, VELOCIDADPIKACHU,imagenPikachu,205,205,40,40,40,40);
+		ARRAYPOKEMON.push(pikachuJugador);
+	}
 	
-	ARRAYPOKEMON.push(pikachu);
-	ARRAYPOKEMON.push(pikachuIA);
-	
+	if(true){
+		let pikachuIA = new Pokemon(Math.random() * 460, YMAQUINA, VELOCIDADPIKACHU,imagenPikachu,10,20,40,40,40,40);
+		ARRAYPOKEMON.push(pikachuIA);
+	}
+
 	// Lanzamos la animación
 	id= setInterval(pintaRectangulo, 1000/100);
 	
